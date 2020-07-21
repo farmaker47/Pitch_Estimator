1a17e82810c987f9d596d001af9c0904cda97bc9
farmaker47 <farmaker47@gmail.com>
2020-07-21 12:08:30 +0300

    Creates karaoke like textview and makes letters blue

--- app/src/main/java/com/george/pitch_estimator/singingFragment/SingingFragment.kt
+++ app/src/main/java/com/george/pitch_estimator/singingFragment/SingingFragment.kt
@@ -3,15 +3,17 @@ package com.george.pitch_estimator.singingFragment
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
-import android.util.Log
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.view.animation.BounceInterpolator
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -19,11 +21,11 @@ import com.george.pitch_estimator.PitchModelExecutor
 import com.george.pitch_estimator.R
 import com.george.pitch_estimator.SingRecorder
 import com.george.pitch_estimator.databinding.FragmentFirstBinding
-import org.koin.android.ext.android.bind
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.getKoin
 import org.koin.android.viewmodel.ext.android.viewModel
 import java.io.File
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -43,10 +45,12 @@ class SingingFragment : Fragment() {
     )
 
     //update interval for widget
-    val UPDATE_INTERVAL = 2048L
+    val UPDATE_INTERVAL_INFERENCE = 2048L
+    val UPDATE_INTERVAL_KARAOKE = 340L
 
     //Handler to repeat update
     private val updateWidgetHandler = Handler()
+    private val updateKaraokeHandler = Handler()
 
     //runnable to loop every 2 seconds with writing sound and infering
     private var updateWidgetRunnable: Runnable = Runnable {
@@ -59,13 +63,44 @@ class SingingFragment : Fragment() {
             val handler = Handler()
             handler.postDelayed({
                 viewModel.stopSinging()
-            }, UPDATE_INTERVAL)
+            }, UPDATE_INTERVAL_INFERENCE)
 
             // Re-run it after the update interval
-            updateWidgetHandler.postDelayed(updateWidgetRunnable, UPDATE_INTERVAL)
+            updateWidgetHandler.postDelayed(updateWidgetRunnable, UPDATE_INTERVAL_INFERENCE)
 
         }
 
+    }
+
+    //runnable to loop every 2 seconds with writing sound and infering
+    private var updateKaraokeRunnable: Runnable = Runnable {
+        run {
+
+            try {
+                for (i in 1..17) {
+
+                    val handler = Handler()
+                    handler.postDelayed({
+                        val WordtoSpan: Spannable = SpannableString(getString(R.string.song_lyrics))
+                        WordtoSpan.setSpan(
+                            ForegroundColorSpan(Color.BLUE),
+                            1,
+                            5 * i,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                        binding.textviewKaraoke.text = WordtoSpan
+
+                        if(i==17){
+                            singingStopped()
+                        }
+
+                    }, UPDATE_INTERVAL_KARAOKE * i)
+
+                }
+            }finally {
+                //singingStopped()
+            }
+        }
     }
 
     override fun onCreateView(
@@ -84,21 +119,18 @@ class SingingFragment : Fragment() {
         binding.buttonForSinging.setOnClickListener {
 
             if (viewModel._singingRunning) {
-                // Remove callback to stop collecting sound
-                updateWidgetHandler.removeCallbacks(updateWidgetRunnable)
-
-                binding.buttonAnimated.clearAnimation()
-
-                //binding.buttonForSinging.text = "Start singing"
-                Toast.makeText(activity,"Singing has stopped",Toast.LENGTH_LONG).show()
+                singingStopped()
             } else {
                 // Start animation
                 animateSharkButton()
                 // Start immediately
                 updateWidgetHandler.postDelayed(updateWidgetRunnable, 0)
 
+                // Start karaoke
+                updateKaraokeHandler.postDelayed(updateKaraokeRunnable, 0)
+
                 //binding.buttonForSinging.text = "Stop singing"
-                Toast.makeText(activity,"Singing has started",Toast.LENGTH_LONG).show()
+                Toast.makeText(activity, "Singing has started", Toast.LENGTH_LONG).show()
 
 
             }
@@ -110,7 +142,26 @@ class SingingFragment : Fragment() {
         // Generate folder for saving .wav later
         generateFolder()
 
+        /*val WordtoSpan: Spannable = SpannableString(getString(R.string.song_lyrics))
+        WordtoSpan.setSpan(
+            ForegroundColorSpan(Color.BLUE),
+            0,
+            13,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.textviewKaraoke.text = WordtoSpan*/
+
         return binding.root
+    }
+
+    private fun singingStopped() {
+        // Remove callback to stop collecting sound
+        updateWidgetHandler.removeCallbacks(updateWidgetRunnable)
+
+        binding.buttonAnimated.clearAnimation()
+
+        //binding.buttonForSinging.text = "Start singing"
+        Toast.makeText(activity, "Singing has stopped", Toast.LENGTH_LONG).show()
     }
 
     private fun animateSharkButton() {
