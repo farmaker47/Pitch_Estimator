692584d07388b11f6f92e2aab1b0c385a9cb254d
farmaker47 <farmaker47@gmail.com>
2020-08-16 10:34:54 +0300

    Makes minor changes

--- app/src/main/java/com/george/pitch_estimator/singingFragment/SingingFragment.kt
+++ app/src/main/java/com/george/pitch_estimator/singingFragment/SingingFragment.kt
@@ -1,5 +1,21 @@
 package com.george.pitch_estimator.singingFragment
 
+
+/*Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
@@ -47,10 +63,6 @@ class SingingFragment : Fragment() {
         Manifest.permission.RECORD_AUDIO,
         Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
-
-    // Update interval for widget
-    val UPDATE_INTERVAL_INFERENCE = 2048L
-    val UPDATE_INTERVAL_KARAOKE = 400L
 
     // Handler to repeat update
     private val updateWidgetHandler = Handler()
@@ -318,5 +330,8 @@ class SingingFragment : Fragment() {
 
     companion object {
         private const val TIME_DELAY_FOR_NOTES = 555L
+        // Update interval for widget
+        private const val UPDATE_INTERVAL_INFERENCE = 2048L
+        private const val UPDATE_INTERVAL_KARAOKE = 400L
     }
 }
